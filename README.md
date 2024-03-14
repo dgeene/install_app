8efbe20f5d0514e7d9a8c7c5209da238fbe37cb2
dgeene <david.geene@gmail.com>
2024-03-13 20:37:46 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Install App
+Installs an .apk or .ipa file onto a phone.
+
+## Problem Statement
+I have a build of a mobile app and many phones connected to my computer. I want to point an app file to a phone and have it installed without remembering which command flags to use.
+
+## Install
+
+## TODO
+- Handle multiple phone connections
+- Handle android unauthorzed devices
+- handle when no devices are returned
+- handle non phone adb devices
+- output device names in addition to uid
+- handle uninstalling the app first
+- progress bar for installs
+- add as command line util https://python-packaging.readthedocs.io/en/latest/command-line-scripts.html#the-console-scripts-entry-point or https://python-poetry.org/docs/pyproject/#scripts
+
+
+
+ios-deploy -j outputs invalid json when multiple devices are present
---
+++ install_app.py
@@ -0,0 +1,128 @@
+import os
+import re
+import argparse
+import subprocess
+from shutil import which
+
+
+def list_ios_devices(ios_deploy: str) -> list:
+    """ Use ios-deploy -c to list out connected devices
+
+    Parse the string output. ios-deploy outputs malformed json
+     when using the -j flag when multiple devices are connected
+     thus why we use string parsing.
+    """
+    devices = []
+    uid_pattern = r"Found\s(.*?)\("
+    completed = subprocess.run([ios_deploy, '-c'], capture_output=True, text=True)
+    if completed.returncode > 0:
+        # error 255 here if no devices connected
+        print('Error listing connected ios devices')
+        print(completed.stderr)
+    else:
+        out = completed.stdout.splitlines()
+        for line in out:
+            uid_match = re.search(uid_pattern, line)
+            if uid_match:
+                devices.append(uid_match[1].strip())
+    return devices
+
+def list_android_devices(adb: str) -> list:
+    """ Use adb devices to get a list of android devices
+
+    Skips unauthorized devices
+    """
+    devices = []
+    completed = subprocess.run([adb, 'devices'], capture_output=True, text=True)
+    if completed.returncode > 0:
+        print('Error listing connected android devices')
+        print(completed.stderr)
+        return None
+    lines = completed.stdout.splitlines()
+    for line in lines:
+        if '\tdevice' in line: # few gotchas like 'unauthorized'
+            devices.append(line.split('\t')[0])
+    return devices
+
+def install_ios_device(ios_deploy: str, ios_device: str, app: str) -> None:
+    """ Use ios-deploy to install the app
+
+    """
+    print(f'Attemping install of {app} onto {ios_device}')
+    completed = subprocess.run([ios_deploy, '-i', ios_device, '-b', app], capture_output=True, text=True)
+    if completed.returncode > 0:
+        print('Error installing onto ios device')
+        print(completed.stderr)
+    else:
+        print('Successfully installed ios app')
+
+def install_android_device(adb: str, android_device: str, app: str) -> None:
+    """ Use adb to install the app
+
+    """
+    print(f'Attemping install of {app} onto {android_device}')
+    completed = subprocess.run([adb, '-s', android_device, 'install', app], capture_output=True, text=True)
+    if completed.returncode > 0:
+        print('Error installing onto android device')
+        print(completed.stderr)
+    else:
+        print('Successfully installed android app')
+
+def handle_device_selection(device_list: list) -> str:
+    """ Prompt to select which device to install the app to
+
+    """
+    if len(device_list) > 1:
+        prompt = "Select a mobile device to install to:\n"
+        for i, d in enumerate(device_list):
+            prompt += f"{i} :: {d}\n"
+        print(prompt)
+        while True:
+            try:
+                answer = int(input('-->'))
+                device_list[answer]
+                break
+            except:
+                print('Invalid option')
+        return device_list[answer]
+    else:
+        return device_list.pop()
+
+def main(ios_deploy_path, adb_path, app_path, file_ext) -> None:
+    if file_ext[1] == '.ipa':
+        devices = list_ios_devices(ios_deploy_path)
+        choice = handle_device_selection(devices)
+        install_ios_device(ios_deploy_path, choice, app_path)
+    elif file_ext[1] == '.apk':
+        devices = list_android_devices(adb_path)
+        choice = handle_device_selection(devices)
+        install_android_device(adb_path, choice, app_path)
+    else:
+        print('Invalid platform')
+
+
+if __name__ == '__main__':
+    ios_deploy_path = which('ios-deploy')
+    adb_path = which('adb')
+    if not ios_deploy_path:
+        print('ios-deploy needs to be installed and available on PATH')
+        exit(1)
+    if not adb_path:
+        print('adb needs to be installed and available on PATH')
+        exit(1)
+
+    parser = argparse.ArgumentParser(prog="mobile_installer", 
+                                     description="Utility function to install an app on android or ios")
+    parser.add_argument('appfile', help='The path to either .ios or .apk file')
+    args = parser.parse_args()
+    app_path = args.appfile
+
+    if not os.path.exists(app_path):
+        print('The app path provided does not exist')
+        exit(1)
+    file_ext = os.path.splitext(app_path)
+    if file_ext[1] not in ['.ipa', '.apk']:
+        print('Invalid file type. Must be .apk or .ipa')
+        exit(1)
+
+    main(ios_deploy_path, adb_path, app_path, file_ext)
